bc338a2777e7a926e39f6224e917cb7847de0ecf
Kyla Moore <001078968@tarleton.edu>
2025-04-27 15:54:15 -0500

    Add files via upload

---
+++ HW27.cu
@@ -0,0 +1,32 @@
+// Name:
+// CPU random walk. 
+// nvcc HW27.cu -o temp
+
+/*
+ What to do:
+ Create a function that returns a random number that is either -1 or 1.
+ Start at 0 and call this function to move you left (-1) or right (1) one step each call.
+ Do this 10000 times and print out your final position.
+*/
+
+// Include files
+
+
+// Defines
+
+
+// Globals
+
+
+// Function prototypes
+
+
+int main(int, char**);
+
+
+int main(int argc, char** argv)
+{
+	
+	return 0;
+}
+
